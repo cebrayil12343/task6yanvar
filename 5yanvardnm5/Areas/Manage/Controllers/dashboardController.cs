2d2a03881b57179e3055dd1f83542ed9b5449ae1
cebrayil12343 <tu2azbdnd@code.edu.az>
2023-01-06 01:36:42 +0400

    taskerror

---
+++ 5yanvardnm5/Areas/Manage/Controllers/dashboardController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _5yanvardnm5.Areas.manages.Controllers
+{
+    [Area("Manage")]
+    public class dashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Creat()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ 5yanvardnm5/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using _5yanvardnm5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _5yanvardnm5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
